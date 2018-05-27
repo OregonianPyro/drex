07a6665c6d305d8854c0e063fd22d6014653c41e
Ryan <36577405+OregonianPyro@users.noreply.github.com>
2018-05-27 00:19:16 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,9 +7,3 @@ module.exports.help = {
     usage: '[prefix]ping',
     description: 'Checks the bot\'s connection the Discord API.'
 };
-
-module.exports.help = {
-    name: 'ping',
-    description: 'Checks the bot\'s response time to the API.',
-    usage: '[prefix]ping'
-};
